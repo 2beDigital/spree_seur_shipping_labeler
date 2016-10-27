3d86a504b04467ddf472b79e16769a3b269c233c
picazoH <4004ahaaa@2016>
2016-10-27 19:49:47 +0200

    Credentials model

--- lib/generators/spree_seur_shipping_labeler/templates/spree_seur_shipping_labeler.rb
+++ lib/generators/spree_seur_shipping_labeler/templates/spree_seur_shipping_labeler.rb
@@ -15,22 +15,22 @@ SpreeSeurShippingLabeler.setup do |config|
   # '077': 'CLASSIC',
   # '083': 'SEUR 8:30',
   config.bundle = {
-    ci:             ENV["bundle_ci"],
-    nif:            ENV["bundle_nif"],
-    ccc:            ENV["bundle_ccc"],
-    service:       ENV["bundle_service"],
-    product:       ENV["bundle_product"]
+    ci:            ENV["seur_bundle_ci"],
+    nif:           ENV["seur_bundle_nif"],
+    ccc:           ENV["seur_bundle_ccc"],
+    service:       ENV["seur_bundle_service"],
+    product:       ENV["seur_bundle_product"]
   }
 
-  config.in0.username = ENV["in0_username"],
-  config.in1.password = ENV["in1_password"],
+  config.in0.username = ENV["seur_username"],
+  config.in1.password = ENV["seur_password"],
 
-  # ==> impresionIntegracionConECBWS
-  config.ConECBWS.in2.printer = ENV["ConECBWS_in2_printer"]
-  config.ConECBWS.in3.printer_model = ENV["ConECBWS_in3_printer_model"]
-  config.ConECBWS.in4.ecb_code = ENV["ConECBWS_in4_ecb_code"]
+  # ImpresionIntegracionConECBWS
+  config.ConECBWS.in2.printer = ENV["seur_printer"]
+  config.ConECBWS.in3.printer_model = ENV["seur_printer_model"]
+  config.ConECBWS.in4.ecb_code = ENV["seur_ecb_code"]
 
-  config.franchise = ENV["imp_franchise"],
-  config.seurid    = ENV["imp_seurid"]
+  config.franchise = ENV["seur_franchise"],
+  config.seurid    = ENV["seur_id"]
 
 end
---
+++ lib/spree_seur_shipping_labeler/credentials.rb
@@ -0,0 +1,22 @@
+require 'spree_seur_shipping_labeler/helpers'
+
+module SpreeSeurShippingLabeler
+  class Credentials
+
+    include Helpers
+    attr_reader :username, :password, :seur_printer, :seur_printer_model,
+                :seur_ecb_code, :seur_franchise, :seur_id
+
+    def initialize(options={})
+      requires!(options, :username, :password, :seur_franchise, :seur_id)
+      @username = options[:username]
+      @password = options[:password]
+      @seur_printer_model = options[:seur_printer_model]
+      @seur_ecb_code = options[:seur_ecb_code]
+      @seur_franchise = options[:seur_franchise]
+      @seur_id = options[:seur_id]
+      @mode = options[:mode]
+    end
+
+  end
+end
