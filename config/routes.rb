bbe4de231da93e02e1040e60c36d8bc28ffa4a30
Noel <noel@2bedigital.com>
2017-01-12 08:27:20 +0000

    added necesary routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 Spree::Core::Engine.routes.draw do
   namespace :admin do
     resources :orders do
-        resources :seur_label
+        resources :seur_labels
     end
   end
 
   namespace :api do
     resources :orders do
-	    member do
-	      post :generate_seur_label, to: 'shipping_labels#generate_seur_label'
-	    end
+      resources :shipments do
+  	    member do
+  	      post :generate_seur_label, to: 'shipping_labels#generate_seur_label'
+  	    end
+      end
     end
   end
 end
