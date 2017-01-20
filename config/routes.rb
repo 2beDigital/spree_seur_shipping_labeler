073789461ca8c34dbf03af9520c32e40da13784a
Noel <noel@2bedigital.com>
2017-01-20 10:54:40 +0000

    added new route and translates

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Spree::Core::Engine.routes.draw do
       resources :shipments do
   	    member do
   	      post :generate_seur_label, to: 'shipping_labels#generate_seur_label'
+          post :generate_delivery_tracking, to: 'shipping_labels#generate_delivery_tracking'
   	    end
       end
     end
