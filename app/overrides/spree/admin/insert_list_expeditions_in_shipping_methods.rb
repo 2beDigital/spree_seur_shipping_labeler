51a0d5a90bb29537b36b7ffa9e5f8bd4647d9d45
Noel <noel@2bedigital.com>
2017-02-01 12:45:39 +0000

    bug in routes

--- app/overrides/spree/admin/insert_list_expeditions_in_shipping_methods.rb
+++ app/overrides/spree/admin/insert_list_expeditions_in_shipping_methods.rb
@@ -4,6 +4,6 @@ Deface::Override.new(virtual_path: 'spree/admin/shipping_methods/edit',
 					 text: "<% if @shipping_method.name =~ /^Seur/ %>
 					 		<fieldset>
 							    <legend><%= Spree.t(:show_shipping_status) %></legend>
-							      <%= link_to  Spree.t(:got_to_show_shipping_status), admin_shipping_method_path(@shipping_method), class: 'button fa fa-truck'%>
+							      <%= link_to  Spree.t(:got_to_show_shipping_status), admin_shipping_method_path(@shipping_method), method: :post, class: 'button fa fa-truck'%>
 							  </fieldset>
 							<% end %>")
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Spree::Core::Engine.routes.draw do
           post :show_tracking_shippings, to: 'shipping_methods#show_tracking_shippings'
         end
         member do
-          get :show, to: 'shipping_methods#show'
+          post :show, to: 'shipping_methods#show'
         end
     end
   end
