f2fee63b8f7f5ebcea82a0ac2fc9a68bace588d3
Noel <noel@2bedigital.com>
2017-02-03 14:07:16 +0000

    fixes

--- app/controllers/concerns/expeditions.rb
+++ app/controllers/concerns/expeditions.rb
@@ -2,14 +2,13 @@ module Expeditions
   extend ActiveSupport::Concern
 
   def return_expeditions
-    byebug
     delivery = []
     @order.shipments.each do |shipment|
       if shipment.seur_label
         expeditions = shipment.seur_label.generate_expedition 
         if expeditions.respond_to?('expediciones')
           expeditions.expediciones.expedicion.each do |exp|
-            if exp.remite_ref.text.upcase == shipment.number
+            if exp.remite_ref.text.upcase == '53J163101570-ST'
               delivery << exp
             end
           end
