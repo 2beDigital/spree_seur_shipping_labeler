a4bfef89d3091217e2410f53f2663709d75410dd
Noel <noel@2bedigital.com>
2017-11-28 15:14:03 +0000

    change needed in overrides

--- app/overrides/spree/insert_show_tracking.rb
+++ app/overrides/spree/insert_show_tracking.rb
@@ -1,9 +1,4 @@
 Deface::Override.new(virtual_path: 'spree/orders/show',
 					 name: 'insert_show_tracking',
 					 insert_before: 'p[data-hook="links"]',
-					 text: '<% if !@delivery.blank? %>
-						 		<h3 id="tracking_status"><%= Spree.t(:show_tracking_status) %></h3>
-						 		<div id="tracking">					 								 		
-						 			<%= render partial: ("spree/shared/show_tracking") %>
-						 		</div>
-						 	<% end %>')
+					 partial: 'spree/shared/show_shipping_status')
